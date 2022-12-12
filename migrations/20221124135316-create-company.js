3f52d38fe6f8648a5be92778fb953c3517bfbe43
Antoine-manu <agaudry.lamanu@gmail.com>
2022-12-12 13:07:46 +0100

    Foreing key

--- migrations/20221124135316-create-company.js
+++ migrations/20221124135316-create-company.js
@@ -10,7 +10,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: user,
+          key: 'id'
+        }
       },
       name: {
         type: Sequelize.STRING
--- models/company.js
+++ models/company.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const user = require('./user');
 module.exports = (sequelize, DataTypes) => {
   class Company extends Model {
     /**
@@ -10,7 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.belongsTo(user, {
+        through: "user_id",
+        foreignKey: "id",
+      });
     }
   }
   Company.init({
