9468ea1ce432bc2030040131ad0dc9d6191dd994
Antoine-manu <agaudry.lamanu@gmail.com>
2022-12-15 10:33:07 +0100

    model fix

--- models/tickets_commentaire.js
+++ models/tickets_commentaire.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Tickets, USer}) {
+    static associate({Tickets, User}) {
       this.belongsTo(Tickets, {
         foreignKey: "ticket_id",
         as: "Ticket"
