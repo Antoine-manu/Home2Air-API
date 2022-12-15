89a041a6b5f158b1846c340d9ced85a10ed6cf0d
Antoine-manu <agaudry.lamanu@gmail.com>
2022-12-15 12:01:08 +0100

    admin check middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+const db = require('../models');
+const User = db.User;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const tokenKey = 'k9zo6QGCjIWzpJ1H82yQ'
@@ -31,10 +33,14 @@ function adminCheck(req, res, next) {
 			return res.sendStatus(401)
 		  }
 		  req.user = user;
-          if(user.role != 2){
-              return res.sendStatus(401)
-          }
-		  next();
+          User.findOne({where :{ id: user.user.id }, attributes: ['role_id']})
+	        .then(user => {
+                if(user.role_id != 2){
+                    return res.sendStatus(401)
+                }
+                next();
+            })
+            .catch(error => res.status(500).json({ error , message : 'Probleme user find', user: user.user}));
 	});
 }
 
