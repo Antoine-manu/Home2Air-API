1d7e92f125bb0fad275549d7b278ad9b663d6cfa
Antoine-manu <agaudry.lamanu@gmail.com>
2023-01-17 15:30:20 +0100

    Generation de notification et CRUD de ce qui concerne les notif

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,6 +4,8 @@ const notificationsController = require("../controllers/notificationsController.
 const notificationsConfigController = require("../controllers/notificationsConfigController.js")
 const notificationsIconsController = require("../controllers/notificationsIconsController.js")
 const notificationsSoundsController = require("../controllers/notificationsSoundController.js")
+const notificationsTypesController = require("../controllers/notificationsTypesController.js")
+const notificationsMessagesController = require("../controllers/notificationsMessagesController.js")
 const auth = require('../middleware/auth');
 
 /* POST create notifications. */
@@ -83,6 +85,46 @@ router.post('/notifications-sounds/update/:id', auth, notificationsSoundsControl
 
 /* POST delete notifications sound. */
 router.post('/notifications-sounds/delete', auth, notificationsSoundsController.delete);
+
+
+
+/* POST create notifications type. */
+router.post('/notifications-types/create', auth, notificationsTypesController.create);
+
+/* POST findAll notifications type. */
+router.post('/notifications-types/find-all', auth, notificationsTypesController.findAll);
+
+/* POST findOne notifications type. */
+router.post('/notifications-types/find-by', auth, notificationsTypesController.findBy);
+
+/* POST findOneById notifications type. */
+router.post('/notifications-types/find-one-by-id', auth, notificationsTypesController.findOneById);
+
+/* POST update notifications type. */
+router.post('/notifications-types/update/:id', auth, notificationsTypesController.update);
+
+/* POST delete notifications type. */
+router.post('/notifications-types/delete', auth, notificationsTypesController.delete);
+
+
+
+/* POST create notifications message. */
+router.post('/notifications/messages/create', auth, notificationsMessagesController.create);
+
+/* POST findAll notifications message. */
+router.post('/notifications/messages/find-all', auth, notificationsMessagesController.findAll);
+
+/* POST findOne notifications message. */
+router.post('/notifications/messages/find-by', auth, notificationsMessagesController.findBy);
+
+/* POST findOneById notifications message. */
+router.post('/notifications/messages/find-one-by-id', auth, notificationsMessagesController.findOneById);
+
+/* POST update notifications message. */
+router.post('/notifications/messages/update/:id', auth, notificationsMessagesController.update);
+
+/* POST delete notifications message. */
+router.post('/notifications/messages/delete', auth, notificationsMessagesController.delete);
 
 
 module.exports = router;
