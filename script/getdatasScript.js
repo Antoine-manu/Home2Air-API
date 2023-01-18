2012fb6ba7134c103293d09b344dc7443d2c1d08
Antoine-manu <agaudry.lamanu@gmail.com>
2023-01-18 16:08:59 +0100

    files moved

--- script/getdatasScript.js
+++ script/getdatasScript.js
@@ -1,1 +1,16 @@
-console.log('texxxxxwst')
+const request = require('request');
+
+request.get(
+    {
+        url: 'http://192.168.1.137:5000/datas',
+        json: true
+    },
+    (err, json) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(json);
+            return res.status(200).json(json);
+        }
+    }
+);
