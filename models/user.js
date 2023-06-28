a18642f9013403329dc5bb58a1df769523012c22
antoine-manu <agaudry.lamanu@gmail.com>
2023-06-28 09:44:28 +0200

    merge fix

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Place, Roles, Company, Notifications, Tickets, Sensor, Tickets_commentaire, Invites}) {
+    static associate({Place, Roles, Company, Notifications, Tickets, Sensor, Tickets_commentaire, Invites, Notifications_config}) {
       this.belongsToMany(Place, {
         through: "user_place_list",
         foreignKey: "user_id",
