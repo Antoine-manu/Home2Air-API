47f595eafd20dad3bf037dd895d7c8412bbe19f4
antoine-manu <agaudry.lamanu@gmail.com>
2023-09-06 13:49:14 +0200

    db user place list name

--- migrations/20221124151322-create-user-place-list.js
+++ migrations/20221124151322-create-user-place-list.js
@@ -4,7 +4,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('User_place_list', {
+    await queryInterface.createTable('User_place_lists', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -36,6 +36,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('User_place_list');
+    await queryInterface.dropTable('User_place_lists');
   }
 };
